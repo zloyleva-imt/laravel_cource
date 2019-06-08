73328ec8010fa2053d6e53cd6b7d894f0528941c
zloyleva <zloyleva.imt@gmail.com>
2019-06-08 17:56:16 +0300

    create entity Products

---
+++ laravel/database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Str;
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'sku' => $faker->numberBetween(0,100000),
+        'name' => join(' ', $faker->words(5)),
+        'slug' => Str::slug(join(' ', $faker->words(5))),
+        'description' => $faker->text(200),
+        'price_user' => $faker->randomFloat(2,0,1000),
+        'price_3_opt' => $faker->randomFloat(2,0,1000),
+        'price_8_opt' => $faker->randomFloat(2,0,1000),
+        'price_dealer' => $faker->randomFloat(2,0,1000),
+        'price_vip' => $faker->randomFloat(2,0,1000),
+        'category_id' => $faker->numberBetween(0,150),
+        'stock' => $faker->numberBetween(0,1500),
+        'feature' => $faker->boolean(30),
+        'views' => $faker->numberBetween(0,1500),
+        'sales_count' => $faker->numberBetween(0,1000),
+    ];
+});
---
+++ laravel/database/migrations/2019_06_08_135612_create_products_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedInteger('sku')->unique();
+            $table->string('name');
+            $table->string('slug');
+            $table->mediumText('description')->nullable();
+
+            $table->float('price_user', 7,2);
+            $table->float('price_3_opt', 7,2);
+            $table->float('price_8_opt', 7,2);
+            $table->float('price_dealer', 7,2);
+            $table->float('price_vip', 7,2);
+
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('stock');
+            $table->boolean('feature')->default(false);
+
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('sales_count')->default(0);
+            $table->float('rate')->default(0);
+
+            $table->timestamps();
+
+            $table->index('id');
+            $table->index('slug');
+            $table->index('sku');
+            $table->index('name');
+            $table->index('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
